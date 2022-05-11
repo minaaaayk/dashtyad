558e8b795cb958bad69a86dd173beb2f1e743a49
minaaaayk <mina.yaghubi95@gmail.com>
2022-05-11 14:12:09 +0430

    :zap: add role checking to user routes

--- packages/server/src/routes/API/User.Routes.ts
+++ packages/server/src/routes/API/User.Routes.ts
@@ -8,7 +8,7 @@ export const UserRouter = (app: Application) => {
   app.get("/users", [checkJwt, checkRole([Role.Admin])], UserController.get_All_Users);  
   app
   .route("/user/:id([0-9]+)")
-    .get([checkJwt],UserController.get_One_User) 
-    .put([checkJwt], UserController.Update_One_User)
-    .delete([checkJwt], UserController.delete_One_User);
+    .get([checkJwt, checkRole([Role.Admin])],UserController.get_One_User) 
+    .put([checkJwt, checkRole([Role.Admin])], UserController.Update_One_User)
+    .delete([checkJwt, checkRole([Role.Admin])], UserController.delete_One_User);
 };
