9259b666976f0d74b471be173acc77f3b73bd895
minaaaayk <mina.yaghubi95@gmail.com>
2022-05-18 11:18:39 +0430

    :zap: add `logout` and `login service`

--- packages/server/src/controllers/Auth.Controller.ts
+++ packages/server/src/controllers/Auth.Controller.ts
@@ -1,52 +1,47 @@
 import { User, UserModel, UserType } from "./../models";
-import {registerUserService} from "../services"
+import { 
+  registerUserService,
+  logoutUserService,
+  loginUserService,
+} from "../services"
 import { Request, Response } from "express";
-import { jwtHelpers, redisHelpers } from "../helpers";
 import { IResponseType } from "../Shared";
 
 const login = async (req: Request, res: Response) => {
 
    if (req.body) {
     try{
-      const {
-        password,
-        email,
-        username,
-      } = req.body as UserType; 
+        const { found,  authorized, auth, user } = await loginUserService(req.body);
+        if (!found) {
+          const notFound: IResponseType = {
+            message: 'User not registered.',
+            status: 401,
+            success: false,
+          };
+          res.status(notFound.status);
+          res.send(notFound);
+          return;
+        }
+        if (found && !authorized) {
+          const notAuthorized: IResponseType = {
+            message: 'User not Authorized.',
+            status: 401,
+            success: false,
+          };
+          res.status(notAuthorized.status);
+          res.send(notAuthorized);
+          return;
+        }
 
-      try {
-        const user = await User.findByCredentials({ password ,username, email });
-        //Sing JWT, valid for 1 hour
-       // Generating access & refresh token ( JWT )
-       const accessToken = await jwtHelpers.generateAccessToken({userId: user.id, username: username || email });
-       const refreshToken = await jwtHelpers.generateRefreshToken({userId: user.id, username: username || email });
-       
-       // Saving refreshToken in redisDB
-       
-       await redisHelpers.SET(user.id, refreshToken);
-        res.setHeader("refresh-token", refreshToken);
-        res.setHeader("access-token", accessToken);
-        const successReq: IResponseType = {
-            message: 'change password successfully',
-            status: 200,
-            success: true,
-            response: user,
-            auth: {
-              userId: user.id,
-              refreshToken,
-              accessToken,
-            }
-        };
-        res.status(successReq.status);
-        res.send(successReq);
-      } catch (error) {
-        const notFoundResponse: IResponseType = {
-          message: 'user not found',
-          status: 409,
+        const authorizedResponse: IResponseType = {
+          message: 'User Authorized.',
+          status: 200,
           success: false,
+          auth,
+          response: user,
         };
-        res.status(notFoundResponse.status).send(notFoundResponse);
-      }
+        res.status(authorizedResponse.status);
+        res.send(authorizedResponse);
     }
     catch (error) {
       const response: IResponseType = {
@@ -174,8 +169,38 @@ const changePassword = async (req: Request, res: Response)=>{
   }
 };
 
+const logout = async (req: Request, res: Response) => {
+  try {
+    //Try to validate the token and get data
+    const data = {
+      refreshToken: req.headers['refresh-token'],
+      accessToken: req.headers['access-token'],
+    };
+    await logoutUserService(data);
+
+    const response: IResponseType = {
+      message: 'Logged out',
+      status: 200,
+      success: true,
+    };
+
+    res.status(response.status);
+    res.send(response);
+  } catch (err) {
+    const response: IResponseType = {
+      message: 'Internal Server Error.',
+      status: 501,
+      success: false,
+    };
+    res.status(response.status);
+    res.send(response);
+  }
+};
+
+
 export const AuthController = {
   register,
   login,
   changePassword,
+  logout,
 };
--- packages/server/src/services/auth/index.ts
+++ packages/server/src/services/auth/index.ts
@@ -1,2 +1,4 @@
 export * from "./register.services";
+export * from "./login.services";
+export * from "./logout.services";
 export * from "./jwt.services";
