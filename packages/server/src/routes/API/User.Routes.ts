a4c78c06ba473379b4c1f0724f4c63a83317f6c3
minaaaayk <mina.yaghubi95@gmail.com>
2022-05-21 21:24:06 +0430

    :zap: update user routes

--- packages/server/src/routes/API/User.Routes.ts
+++ packages/server/src/routes/API/User.Routes.ts
@@ -4,10 +4,11 @@ import { checkJwt, checkRole } from "../../middlewares";
 import { Role } from "../../models";
 
 export const UserRouter = (app: Application) => {
-  app.get("/users", [checkJwt, checkRole([Role.Admin])], UserController.get_All_Users);  
+  app.get("/users", [checkJwt, checkRole([Role.Admin])], UserController.get_UsersList);
+  app.post("/user", [checkJwt, checkRole([Role.Admin])],UserController.create_User);
   app
-  .route("/user/:id([0-9]+)")
-    .get([checkJwt, checkRole([Role.Admin])],UserController.get_One_User) 
-    .put([checkJwt, checkRole([Role.Admin])], UserController.Update_One_User)
-    .delete([checkJwt, checkRole([Role.Admin])], UserController.delete_One_User);
+  .route("/user/:id")
+  .get([checkJwt, checkRole([Role.Admin])],UserController.get_User) 
+  .put([checkJwt, checkRole([Role.Admin])], UserController.Update_User)
+  .delete([checkJwt, checkRole([Role.Admin])], UserController.delete_User);
 };
