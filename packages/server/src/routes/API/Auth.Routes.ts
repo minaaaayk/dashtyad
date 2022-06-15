77bfc837ce8936d55a86c251584608ffe515005b
minaaaayk <mina.yaghubi95@gmail.com>
2022-06-15 12:25:01 +0430

    :zap: add `validation` to routes

--- packages/server/src/routes/API/Auth.Routes.ts
+++ packages/server/src/routes/API/Auth.Routes.ts
@@ -1,10 +1,14 @@
 import { AuthController } from "../../controllers";
 import { Application } from "express";
-import { checkJwt } from "../../middlewares";
+import { checkJwt, AuthValidations, privateHeaderValidation } from "../../middlewares";
 
 export const AuthRouter = (app: Application) => {
-  app.route("/login").post(AuthController.login);
-  app.route("/register").post( AuthController.register);
-  app.put("/change-password", [checkJwt], AuthController.changePassword);
-  app.post('/logout', [checkJwt], AuthController.logout);
+  app.post("/login", AuthValidations.login, AuthController.login);
+  app.post("/register",AuthValidations.register, AuthController.register);
+  app.put(
+    "/change-password",
+    [privateHeaderValidation, AuthValidations.changePassword , checkJwt],
+    AuthController.changePassword
+  );
+  app.post('/logout', [privateHeaderValidation, checkJwt], AuthController.logout);
 };
--- packages/server/src/routes/API/User.Routes.ts
+++ packages/server/src/routes/API/User.Routes.ts
@@ -1,14 +1,23 @@
 import { UserController } from "../../controllers";
 import { Application } from "express";
 import { checkJwt, checkRole } from "../../middlewares";
+import { privateHeaderValidation, UserValidations } from "../../middlewares";
 import { Role } from "../../models";
 
 export const UserRouter = (app: Application) => {
-  app.get("/users", [checkJwt, checkRole([Role.Admin])], UserController.get_UsersList);
-  app.post("/user", [checkJwt, checkRole([Role.Admin])],UserController.create_User);
+  app.get(
+    "/users",
+    [privateHeaderValidation, checkJwt, checkRole([Role.Admin])],
+    UserController.get_UsersList
+  );
+  app.post(
+    "/user",
+    [privateHeaderValidation, checkJwt, UserValidations.createUser, checkRole([Role.Admin])],
+    UserController.create_User
+  );
   app
   .route("/user/:id")
-  .get([checkJwt, checkRole([Role.Admin])],UserController.get_User) 
-  .put([checkJwt, checkRole([Role.Admin])], UserController.Update_User)
-  .delete([checkJwt, checkRole([Role.Admin])], UserController.delete_User);
+  .get([privateHeaderValidation, checkJwt, checkRole([Role.Admin])],UserController.get_User) 
+  .put([privateHeaderValidation, checkJwt, UserValidations.updateUser ,checkRole([Role.Admin])], UserController.Update_User)
+  .delete([privateHeaderValidation, checkJwt, checkRole([Role.Admin])], UserController.delete_User);
 };
